76950f62c65c3c51a3dd28c7df89f2215b7f931a
liquid11 <orangevolution@yahoo.com>
2015-01-04 02:56:46 +0500

    Initial commit

---
+++ helloWorld.py
@@ -0,0 +1,17 @@
+__author__ = 'ARSLAN'
+
+print('CapyBara is the worlds largest redeont')
+
+print("Capybara Lives inside the  \nsouth America")
+
+print("""this is very very
+
+strange""")
+
+print('this is a double quote" "'+"and this is a single quote ' '")
+
+print("this is also another neat trick \"\"")
+
+print("can i do this \does this work  ")
+
+print("print this \\news")
